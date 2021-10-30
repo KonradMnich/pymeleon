fc89e3b150fa625f7090f6068313816e85d2c196
KonradMnich <konrad.mnich@km.kongsberg.com>
2021-10-30 16:05:04 +0200

    Enable multiple modifications with one command, add enums

--- examples/change_font_properties.py
+++ examples/change_font_properties.py
@@ -1,16 +1,21 @@
 """Change font properties of the elements of axis."""
-from pymeleon.font_modifiers import modify_font
+from pymeleon.font_modifiers import *
 import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
+    # create a plot
     fig, ax = plt.subplots()
-
     ax.plot([0, 1], [0, 1])
-    modify_font(ax, "xticks", "size", 20)
-    modify_font(ax, "xticks", "family", "serif")
-    modify_font(ax, "xticks", "slant", "italic")
-    ax.set_ylabel("my text")
-    modify_font(ax, "ylabel", "family", "fantasy")
-    modify_font(ax, "ylabel", "size", 25)
+    ax.set_title("my title")
+    ax.set_ylabel("my label")
+
+    # modify the plot
+    modify_font(ax, Location.XTICKS, Feature.COLOR, "olive")
+    modify_multiple_elements(ax, "all", (Feature.SIZE, 20))
+    modify_multiple_features(
+        ax, Location.YLABEL, [(Feature.ALPHA, 0.5), (Feature.SLANT, "italic")]
+    )
+
+    fig.tight_layout()
     fig.show()
--- pymeleon/font_modifiers.py
+++ pymeleon/font_modifiers.py
@@ -1,6 +1,23 @@
 """The module contains the code that allows to change properties of the text
 objects of the figure.
 """
+from enum import Enum, auto
+
+
+class Location(Enum):
+    TITLE = auto()
+    XLABEL = auto()
+    YLABEL = auto()
+    XTICKS = auto()
+    YTICKS = auto()
+
+
+class Feature(Enum):
+    COLOR = "color"
+    ALPHA = "alpha"
+    SIZE = "size"
+    FAMILY = "family"
+    SLANT = "slant"
 
 
 def modify_font(ax, location, feature_name, feature_value):
@@ -10,11 +27,10 @@ def modify_font(ax, location, feature_name, feature_value):
     ----------
     ax : matplotlib.axes.Axes
         Object containing text to be modified
-    location : str
-        One of the following ["title", "xlabel", "ylabel", "xticks", "yticks",
-        legend"]
-    feature_name : str
-        Supported features: "color", "alpha", "size", "family", "slant"
+    location : Location
+        For the list of supported features see Location definition.
+    feature_name : Feature
+        For the list of supported features see Feature definition.
     feature_value : str or float
         New value for the changed feature
 
@@ -28,23 +44,63 @@ def modify_font(ax, location, feature_name, feature_value):
     For the list of available text features check out Matplotlib documentation:
     https://matplotlib.org/stable/tutorials/text/text_props.html (2021-10-30)
     """
-    if location == "title":
+    if location == Location.TITLE:
         my_texts = [ax.title]
-    elif location == "xlabel":
+    elif location == Location.XLABEL:
         my_texts = [ax.xaxis.label]
-    elif location == "ylabel":
+    elif location == Location.YLABEL:
         my_texts = [ax.yaxis.label]
-    elif location == "xticks":
+    elif location == Location.XTICKS:
         my_texts = [tick.label for tick in ax.xaxis.majorTicks]
-    elif location == "yticks":
+    elif location == Location.YTICKS:
         my_texts = [tick.label for tick in ax.yaxis.majorTicks]
     else:
         raise ValueError("Location not suported.")
 
     for text in my_texts:
-        if feature_name in ["color", "alpha"]:
-            setattr(text, f"_{feature_name}", feature_value)
+        if feature_name in [Feature.COLOR, Feature.ALPHA]:
+            setattr(text, f"_{feature_name.value}", feature_value)
         else:
-            setattr(text._fontproperties, f"_{feature_name}", feature_value)
+            setattr(text._fontproperties, f"_{feature_name.value}", feature_value)
 
+    return ax
+
+
+def modify_multiple_elements(ax, locations="all", feature=(Feature.FAMILY, "serif")):
+    """Modify multiple elements.
+
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes
+        Object containing text to be modified
+    locations : list(Location) or str, optional
+        For the list of supported features see Location definition.
+        By default all available locations are modified.
+    feature : tuple, optional
+        Tuple composed of Feature name and its value: (Feature.COLOR, "red").
+        By default the font family is changed to serif.
+    """
+    if locations == "all":
+        locations = [location for location in Location]
+    for location in locations:
+        modify_font(ax, location, feature[0], feature[1])
+    return ax
+
+
+def modify_multiple_features(ax, location, features):
+    """Modify multiple features.
+
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes
+        Object containing text to be modified
+    location : Location
+        For the list of supported features see Location definition.
+        By default all available locations are modified.
+    features : list(tuple)
+        List of tuples composed of feature name and its value:
+        [(Feature.COLOR, "red"), (Feature.FAMILY, "serif")].
+    """
+    for feature in features:
+        modify_font(ax, location, feature[0], feature[1])
     return ax
